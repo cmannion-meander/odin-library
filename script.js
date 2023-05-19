090cf2d95d76baf3207b915a5d5ff1df650cb23c
cmannion-meander <cmannion@meanderhq.com>
2023-05-19 12:32:36 -0400

    Complete Library project

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ function Book(title, author, pages, read) {
   this.read = read
 }
 
-const content = document.querySelector('#content');
+
 
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  const book = document.createElement('div');
-  book.classList.add('book');
-  book.textContent = `${newBook.title} by ${newBook.author}, ${newBook.pages} pages, ${newBook.read}`;
-  content.appendChild(book);
+  // const book = document.createElement('div');
+  // book.classList.add('book');
+  // book.textContent = `${newBook.title} by ${newBook.author}, ${newBook.pages} pages, ${newBook.read}`;
+  // content.appendChild(book);
 }
 
 addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 275, "read")
@@ -43,5 +43,56 @@ function submitClick(event) {
   pages = document.getElementById("pages").value;
   read = document.querySelector('input[name="read"]:checked').value;
   addBookToLibrary(title, author, pages, read);
+  displayBooks();
   closeForm()
 }
+
+const content = document.querySelector('#content');
+
+function displayBooks() {
+  const elements = document.querySelectorAll('.book');
+
+  elements.forEach((element) => {
+    element.remove();
+  });
+  for (let i = 0; i < myLibrary.length; i++) {
+    const book = document.createElement('div');
+    book.classList.add('book');
+    newBook = myLibrary[i];
+    book.textContent = `${newBook.title} by ${newBook.author}, ${newBook.pages} pages, ${newBook.read}`;
+    book.setAttribute('id',i);
+    // Add new div with read and remove options
+    const tools = document.createElement('div');
+    tools.classList.add('tools');
+    let add = document.createElement("img");
+    add.src = 'book-check.svg';
+        if (newBook.read == "to be read") {
+      add.setAttribute('id',i);
+      add.addEventListener("click", readBook, false);
+      tools.appendChild(add);
+    };
+    let remove = document.createElement("img");
+    remove.src = 'book-remove.svg';
+    remove.setAttribute('id',i);
+    remove.addEventListener("click", removeBook, false);
+
+    tools.appendChild(remove);
+    book.appendChild(tools);
+    content.appendChild(book);
+  };
+}
+
+function readBook(id) {
+  bookNum = id.currentTarget.id;
+  book = myLibrary[bookNum];
+  book.read = "read";
+  displayBooks();
+}
+
+function removeBook(id) {
+  bookNum = id.currentTarget.id;
+  myLibrary.splice(bookNum, 1);
+  displayBooks();
+}
+
+displayBooks();
